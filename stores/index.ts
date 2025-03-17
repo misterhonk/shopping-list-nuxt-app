d08dd8ccbdb4612863c1e86b58782fbb14b01fab
Martin Melcher <mm@mister-honk.de>
2025-03-17 13:12:11 +0100

    removed unused files

--- stores/index.ts
+++
@@ -1,5 +0,0 @@
-// Diese Datei dient nur als Referenz, da Nuxt 3 Auto-Imports verwendet.
-// Direkte Imports von Stores werden bevorzugt.
-
-// Hinweis: Wenn Sie den useCategoryStore benötigen, importieren Sie ihn direkt wie folgt:
-// import { useCategoryStore } from '~/stores/category';
