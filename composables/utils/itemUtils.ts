fe37bc20234bfe5e13ab14075589b71fdcfd98ef
root <root@2ef838bb8fe0>
2025-03-28 16:14:51 +0000

    fix: correct logger variable name in itemUtils

--- composables/utils/itemUtils.ts
+++ composables/utils/itemUtils.ts
@@ -2,7 +2,7 @@ import { createLogger } from '~/utils/logger';
 
 // Logger initialisieren (für Entwicklungszwecke)
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const _logger = createLogger('itemUtils');
+const logger = createLogger('itemUtils');
 
 /**
  * Prüft, ob ein Item zu einer bestimmten Kategorie-ID gehört
